d372cce722f27c58d796ad163031fc419919a4f8
Michael Pomatto <mike@pomatto.com>
2021-07-10 01:20:08 -0400

    modified index.js

--- index.js
+++ index.js
@@ -1,14 +1,5 @@
-const http = require('http');
-const hostname = '127.0.0.1';
-const port = 3000;
-
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Updated Information\n');
-});
-
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
-});
+function sum(a,b) {
+  return a+b;
+}
+module.exports = sum;
     
