ac8ab9a9c775a3825684396d6d3a4a3dd633f8cf
Michael Pomatto <mike@pomatto.com>
2021-07-10 01:31:07 -0400

    Updated with SUM.js

--- sum.test.js
+++ sum.test.js
@@ -4,10 +4,3 @@ test ('sum of two numbers', () => {
   expect(sum(1, 2)).toBe(3);
 });
 
-test('sum of three numbers', () => {
-  expect(sum(1, 2, 3)).toBe(6);
-});
-
-test('sum of four numbers', () => {
-  expect(sum(1, 2, 3, 4)).toBe(10);
-});
